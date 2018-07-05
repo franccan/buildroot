3c90bc637ca4b4a2287abef1c52a94c2563d4e3c
Einar Jon Gunnarsson <tolvupostur@gmail.com>
2018-07-05 08:57:53 +0200

    package/ti-sgx-libgbm: new package
    ti-sgx-libgbm is a required package for ti-sgx-um in SDK 4.00
    
    Signed-off-by: Einar Jon Gunnarsson <tolvupostur@gmail.com>
    [Thomas:
     - add missing dependencies on libdrm and udev
     - turn into a provider of libgbm]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/ti-sgx-libgbm/ti-sgx-libgbm.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# ti-sgx-libgbm
+#
+################################################################################
+
+# This is the current "next" branch, which corresponds to SDK 04.00.00.00
+TI_SGX_LIBGBM_VERSION = 43cc786d0e5b8274fa93048c7f3bb8d6b62522db
+TI_SGX_LIBGBM_SITE = git://git.ti.com/glsdk/libgbm.git
+TI_SGX_LIBGBM_INSTALL_STAGING = YES
+TI_SGX_LIBGBM_AUTORECONF = YES
+TI_SGX_LIBGBM_LICENSE = MIT
+TI_SGX_LIBGBM_LICENSE_FILES = debian/copyright
+TI_SGX_LIBGBM_PROVIDES = libgbm
+TI_SGX_LIBGBM_DEPENDENCIES = wayland libdrm udev
+
+$(eval $(autotools-package))
