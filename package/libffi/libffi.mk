8355d6cba4b2d4b2900d65661f22d1f39697f228
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2020-01-15 14:21:48 +0100

    package/libffi: add support for the K1 architecture
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libffi/libffi.mk
+++ package/libffi/libffi.mk
@@ -4,8 +4,13 @@
 #
 ################################################################################
 
+ifeq ($(BR2_k1),y)
+LIBFFI_VERSION = eacb2cfddcab2a758b20b8b4da84af2166c3c2f9
+LIBFFI_SITE = $(call github,kalray,libffi,$(LIBFFI_VERSION))
+else
 LIBFFI_VERSION = v3.3-rc0
 LIBFFI_SITE = $(call github,libffi,libffi,$(LIBFFI_VERSION))
+endif
 LIBFFI_LICENSE = MIT
 LIBFFI_LICENSE_FILES = LICENSE
 LIBFFI_INSTALL_STAGING = YES
